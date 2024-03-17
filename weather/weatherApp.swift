b7d1e99d8de523e34b3e202128adf6ec44950dae
Rakib <monsur.rakib@maiki.co>
2024-03-17 20:05:56 +0600

    initial commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,21 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Rakib on 3/17/24.
+//
+
+import SwiftUI
+import Home
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            HomeView()
+            
+        }
+    }
+}
+
